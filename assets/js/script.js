a778d5b3c402002c020063cc2d967508b4c97dbf
Joe Riley <joeyriley@gmail.com>
2021-02-08 10:26:03 -0700

    Add states and questions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,27 @@
-const questions = [
-    {
-        question: 'This is a question that will not stump anyone but it should fill up enough space to try using a longer question than "Is this true or false?".',
-        possibleAnswers: [
-            {
-                text: 'Answer 1',
-                isCorrect: true,
-            },
-            {
-                text: 'Answer 2',
-                isCorrect: false,
-            },
-            {
-                text: 'Answer 3',
-                isCorrect: false,
-            },
-            {
-                text: 'Answer 4',
-                isCorrect: false,
-            },
-        ]
-    },
-]
-    
+const testingStates = {
+    START,
+    PASSED,
+    FAILED,
+    QUIT,
+    TIMEOUT,
+};
 
-let questionArea = document.querySelector('.question_area');
+let testState = testingStates.START;
 
+const timmer = (minutes) => {
+    const timeEl = document.querySelector('#time');
+    const interval = setInterval(() => {
+        timeEl.innerHTML -= 1;
+    }, 1000);
+}
 
-let timmer = () => {
+const presentQuestions = () => {
+    const questionArea = document.querySelector('#question_area');
+}
+
+const startQuiz = (event) => {
 
 }
 
-let presentQuestions = () => {
-}
-
-let startQuiz = (event) => {
-    console.log("pressed button");
-
-}
-
+// timmer();
 questionArea.addEventListener('click', startQuiz);
---
+++ assets/js/states.js
@@ -0,0 +1,48 @@
+const states = {
+  start: {
+    heading: 'Coding Quiz',
+    paragraph: 'A quiz on coding by someone who has not learned all of the in and outs of coding in 10 years of trying to learn.',
+    button1: 'START'
+  },
+  finish: {
+    heading: 'You completed the quiz!',
+    paragraph: 'You scored: ',
+    button1: 'Scoreboard',
+    button2: 'Start Over',
+  },
+  quit: {
+    heading: 'Quit Now?',
+    paragraph: 'Are you sure you want to quit?',
+    button1: 'No, keep going',
+    button2: 'Quit for now',
+  },
+  timeout: {
+    heading: 'You ran out of time',
+    paragraph: 'Keep trying to answer all of the questions in the time allowed!',
+    button1: 'Scoreboard',
+    button2: 'Try Again',
+  },
+  questions: [
+    {
+      question: 'This is a question that will not stump anyone but it should fill up enough space to try using a longer question than "Is this true or false?".',
+      possibleAnswers: [
+        {
+          text: 'Answer 1',
+          isCorrect: true,
+        },
+        {
+          text: 'Answer 2',
+          isCorrect: false,
+        },
+        {
+          text: 'Answer 3',
+          isCorrect: false,
+        },
+        {
+          text: 'Answer 4',
+          isCorrect: false,
+        },
+      ],
+    },
+  ]
+}
